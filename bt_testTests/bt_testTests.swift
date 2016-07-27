7289e13d59abc3847d08556b2642b54f2e3aae42
Rafael de los Santos <rafael.delossantos@rafaeldelossantos.local>
2016-07-27 13:21:13 -0400

    Initial Commit

---
+++ bt_testTests/bt_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  bt_testTests.swift
+//  bt_testTests
+//
+//  Created by Rafael de los Santos on 7/27/16.
+//  Copyright © 2016 Rafael de los Santos. All rights reserved.
+//
+
+import XCTest
+@testable import bt_test
+
+class bt_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
