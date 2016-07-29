2b5a2d85234b6b4cc1d772d5c77ac7d5575a6ffe
Rafael de los Santos <rafael.delossantos@rafaeldelossantos.local>
2016-07-29 15:54:59 -0400

    i made a change

--- bt_test/ClockView.swift
+++ bt_test/ClockView.swift
@@ -1,9 +1,61 @@
 //
 //  ClockView.swift
-//  bt_test
+//  ClockVisuals
 //
-//  Created by Rafael de los Santos on 7/29/16.
-//  Copyright © 2016 Rafael de los Santos. All rights reserved.
+//  Created by Jacob Macdonald on 7/29/16.
+//  Copyright © 2016 Pentagram. All rights reserved.
 //
 
 import Foundation
+import UIKit
+
+let π : CGFloat = CGFloat(M_PI)
+
+@IBDesignable class ClockView : UIView {
+    
+    @IBInspectable var outlineColor: UIColor = UIColor.blueColor()
+    @IBInspectable var counterColor: UIColor = UIColor.orangeColor()
+    
+    private let LEDs = 120
+    private var sections: [[CGFloat]] = []
+    
+    override func drawRect(rect: CGRect) {
+        print("hi")
+        layer.cornerRadius = frame.size.width / 2
+        clipsToBounds = true
+        
+        let center = CGPoint(x:bounds.width/2, y: bounds.height/2)
+        let radius: CGFloat = max(bounds.width, bounds.height)
+        let arcWidth: CGFloat = 11
+        let path = UIBezierPath(arcCenter: center,
+                                radius: radius / 2 - arcWidth / 2,
+                                startAngle: 0,
+                                endAngle: 2 * π,
+                                clockwise: true)
+        path.lineWidth = arcWidth
+        outlineColor.setStroke()
+        path.stroke()
+        
+        
+        for section in sections {
+            let start = section[0]
+            let length = section[1]
+            let startAngle: CGFloat = (2 * π / CGFloat(LEDs) * CGFloat(start) + 3 * π / 2) % (2 * π)
+            let endAngle: CGFloat = (2 * π / CGFloat(LEDs) * CGFloat(start + length) + 3 * π / 2) % (2 * π)
+            let path = UIBezierPath(arcCenter: center,
+                                    radius: radius / 2 - arcWidth / 2,
+                                    startAngle: startAngle,
+                                    endAngle: endAngle,
+                                    clockwise: true)
+            path.lineWidth = arcWidth
+            //path.lineCap = kCALineCapRound
+            counterColor.setStroke()
+            path.stroke()
+        }
+    }
+    
+    func update(sections: [[CGFloat]]) {
+        self.sections = sections
+        setNeedsDisplay()
+    }
+}
--- bt_test/ViewController.swift
+++ bt_test/ViewController.swift
@@ -9,10 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var labels:[UILabel] = []
+    @IBOutlet weak var clock: ClockView!
+    let labelStartY:CGFloat = 30
+    let labelJumpY:CGFloat = 30
+    var nrf:NRFManager!
+    var v = "👍🏻"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.blackColor()
+        print(v)
+        
+        nrf = NRFManager(
+            onConnect: {
+                print("C: ★ Connected")
+            },
+            onDisconnect: {
+                print("C: ★ Disconnected")
+            },
+            onData: {
+                (data:NSData?, string:String?)->() in
+                print("C: ⬇ Received data - String: \(string) - Data: \(data)")
+            },
+            autoConnect: true
+        )
+        
+        nrf.verbose = true
+        clock.update([
+            [ CGFloat(arc4random_uniform(120)), 5 ],
+            [ CGFloat(arc4random_uniform(120)), 10 ]
+            ])
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +49,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func connect(sender: AnyObject) {
+        nrf.connect()
+    }
+    
+    @IBAction func sendMessage(sender: AnyObject) {
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([ .Hour, .Minute, .Second], fromDate: date)
+        let hour = components.hour
+        let minute = components.minute
+        let second = components.second
+        //let month = components.month
+        //let year = components.year
+        //let day = components.day
+        print ("\(hour):\(minute):\(second)")
+        let message = "\(hour):\(minute):\(second):"
+        let result = nrf.writeString(message)
+        print("sending message")
+        print(result)
+        showLabel()
+    }
+    
+    func showLabel() {
+        let label = UILabel(frame: CGRectMake(0, 0, 200, 21))
+        label.center = CGPointMake(160, labelStartY)
+        label.textAlignment = NSTextAlignment.Center
+        label.text = "I'am a test label"
+        self.view.addSubview(label)
+        labels.append(label)
+        UIView.animateWithDuration(2, animations: {label.alpha = 0}, completion:{
+            (finished: Bool) -> Void in
+            for l in self.labels {
+                if l == label {
+                    self.labels.removeAtIndex(self.labels.indexOf(l)!)
+                    l.removeFromSuperview()
+                }
+            }
+        })
+        
+        for l in labels {
+            UIView.animateWithDuration(0.5, animations: {l.center.y = l.center.y + self.labelJumpY})
+        }
+    }
 }
 
